16afbc9430b30f4cd6584cd959183ad324197307
goesderai <goesderai@gmail.com>
2014-09-05 16:45:30 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	lib-zk-bccore/src/main/resources/web/zul/pages/common/ActionButton.zul

--- lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/controller/security/PageDefintionListController.java
+++ lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/controller/security/PageDefintionListController.java
@@ -1,18 +1,24 @@
 package id.co.sigma.zk.ui.controller.security;
 
+import java.awt.event.MouseEvent;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.ForwardEvent;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Textbox;
+
 import id.co.sigma.common.data.query.SimpleQueryFilterOperator;
 import id.co.sigma.common.security.domain.PageDefinition;
 import id.co.sigma.zk.ui.annotations.QueryParameterEntry;
 import id.co.sigma.zk.ui.controller.EditorManager;
 import id.co.sigma.zk.ui.controller.IReloadablePanel;
 import id.co.sigma.zk.ui.controller.base.BaseSimpleListController;
-
-import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.select.annotation.Listen;
-import org.zkoss.zk.ui.select.annotation.Wire;
-import org.zkoss.zul.Button;
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Textbox;
 
 /**
  * controller untuk handler 
@@ -63,7 +69,6 @@ public class PageDefintionListController extends BaseSimpleListController<PageDe
 		super.doAfterCompose(comp);
 		invokeSearch();
 	}
-	
 	@Listen(value="onClick = #btnCari")
 	public void searchClick() {
 		invokeSearch();
@@ -90,11 +95,42 @@ public class PageDefintionListController extends BaseSimpleListController<PageDe
 		
 	}
 	
-	@Override
-	public void deleteData(PageDefinition pDef) {
-		deleteData(pDef, pDef.getId(), "id");
-		reload();
+	
+	@Listen("onEditData = #pageListBox")
+	public void clickEdit(ForwardEvent e) {
+		Component item = e.getOrigin().getTarget().getParent().getParent();
+		if(item instanceof Listitem) {
+			PageDefinition pDef = ((Listitem)item).getValue();
+			EditorManager.getInstance().editData("~./zul/pages/master/security/PageDefinitionEditor.zul", pDef, this);
+		}
 	}
+	
+	@Listen("onDeleteData = #pageListBox")
+	public void clickDelete(ForwardEvent e) {
+		Component item = e.getOrigin().getTarget().getParent().getParent();
+		if(item instanceof Listitem) {
+			PageDefinition pDef = ((Listitem)item).getValue();
+			deleteData(pDef, pDef.getId(), "id");
+			reload();
+		}
+	}
+	
+//	@Listen(value="onClick = #btnHapus")
+//	public void hapusClick() {
+//		
+//	}
+	
+//	@Listen(value="onSelect = #pageListBox")
+//	public void listboxSelected () {
+//		System.out.println("selected");
+//		btnHapus.setVisible(true); 
+//		btnEdit.setVisible(true);
+//	}
+//	
+//	@Listen(value="onClick =#pageListBox")
+//	public void onEditOnGridClick(ForwardEvent evt ) {
+//		Messagebox.show("Test click Grid"); 
+//	}
 	
 
 }
