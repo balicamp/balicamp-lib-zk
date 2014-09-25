fe769a15f14eb67f0e46239bacd475b8b5d74d78
riepentaa <unyil_niz@yahoo.com>
2014-09-25 16:06:17 +0800

    Add Role Maintenance

---
+++ lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/controller/security/RoleController.java
@@ -0,0 +1,103 @@
+package id.co.sigma.zk.ui.controller.security;
+
+import id.co.sigma.common.security.domain.Role;
+import id.co.sigma.common.data.query.SimpleQueryFilterOperator;
+import id.co.sigma.zk.ui.annotations.QueryParameterEntry;
+import id.co.sigma.zk.ui.controller.EditorManager;
+import id.co.sigma.zk.ui.controller.IReloadablePanel;
+import id.co.sigma.zk.ui.controller.base.BaseSimpleListController;
+
+import org.springframework.stereotype.Controller;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Textbox;
+
+/**
+ * 
+ * @author <a href="mailto:rie.anggreani@gmail.com">Arie Anggreani</a>
+ */
+@Controller
+public class RoleController extends BaseSimpleListController<Role> implements IReloadablePanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1003489155768495181L;
+
+	@QueryParameterEntry(
+			filteredField="roleCode" , 
+			queryOperator=SimpleQueryFilterOperator.likeBothSide)
+	@Wire
+	Textbox codeSearch ;
+	@QueryParameterEntry(filteredField="roleDesc" , queryOperator= SimpleQueryFilterOperator.equal)
+	@Wire
+	Textbox descSearch  ;
+	@Wire
+	Button searchButton ;
+	@Wire
+	Listbox listbox ; 
+	@Wire Button resetButton ;
+	@Wire Button addButton ; 
+
+	@Wire Button btnEdit ; 
+	@Wire Button btnHapus ;
+
+
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		// TODO Auto-generated method stub
+		super.doAfterCompose(comp);
+		invokeSearch();
+	};
+	
+	@Override
+	protected Class<? extends Role> getHandledClass() {
+		// TODO Auto-generated method stub
+		return Role.class;
+	}
+
+	@Override
+	public Listbox getListbox() {
+		// TODO Auto-generated method stub
+		return listbox;
+	}
+
+	
+	@Listen(value="onClick = #resetButton")
+	public void resetClick() {
+		codeSearch.setValue("");
+		descSearch.setValue("");
+		invokeSearch();
+	};
+	
+	@Override
+	public void reload() {
+		invokeSearch();
+	}
+	  
+	@Override
+	public void deleteData(Role data) {
+		deleteData(data, data.getId(), "id");
+		reload();
+	}
+	
+	 @Listen(value="onClick = #addButton")
+	    public void clickAdd(){
+	    	Role role = new Role();
+	    	EditorManager.getInstance().addNewData("~./zul/pages/master/security/RoleFormEditor.zul", role, this);
+	 }
+	 
+	 @Listen(value="onClick = #searchButton")
+	 public void click() {
+	        invokeSearch();
+	 }
+
+
+	
+
+	 
+
+}
---
+++ lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/controller/security/RoleEditorController.java
@@ -0,0 +1,46 @@
+package id.co.sigma.zk.ui.controller.security;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Page;
+import org.zkoss.zk.ui.metainfo.ComponentInfo;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Radio;
+import org.zkoss.zul.Radiogroup;
+import org.zkoss.zul.Textbox;
+
+import id.co.sigma.common.security.domain.Role;
+import id.co.sigma.zk.ui.controller.EditorManager;
+import id.co.sigma.zk.ui.controller.base.BaseSimpleDirectToDBEditor;
+
+/**
+ * 
+ * @author <a href="mailto:rie.anggreani@gmail.com">Arie Anggreani</a>
+ */
+public class RoleEditorController extends BaseSimpleDirectToDBEditor<Role>{
+
+	/**
+	 * 
+	 */
+	static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
+			.getLogger(RoleEditorController.class.getName());
+	private static final long serialVersionUID = 7109113124969951147L;
+	
+	
+	@Listen("onClick=#btnCancel")
+    public void onCancel(){
+        EditorManager.getInstance().closeCurrentEditorPanel();
+    }
+	
+	
+	@Override
+	protected void parseEditedData(Component comp) {
+		// TODO Auto-generated method stub
+		super.parseEditedData(comp);
+		editedData.setIsPredefined("N");
+		
+	}
+	
+}
