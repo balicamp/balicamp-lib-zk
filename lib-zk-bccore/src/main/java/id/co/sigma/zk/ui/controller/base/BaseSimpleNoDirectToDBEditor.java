844d4ad164a2bbf4010b3911000d086961ac7800
Pala Windu <pala.windu@gmail.com>
2014-10-02 10:46:18 +0800

    update code (commit ulang)

--- lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/controller/base/BaseSimpleNoDirectToDBEditor.java
+++ lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/controller/base/BaseSimpleNoDirectToDBEditor.java
@@ -2,7 +2,13 @@ package id.co.sigma.zk.ui.controller.base;
 
 import java.util.Map;
 
+import org.zkoss.zhtml.Messagebox;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.select.annotation.Listen;
+
 import id.co.sigma.zk.ZKCoreLibConstant;
+import id.co.sigma.zk.ui.controller.EditorManager;
 import id.co.sigma.zk.ui.controller.ZKEditorState;
 import id.co.sigma.zk.ui.data.ZKClientSideListDataEditorContainer;
 
@@ -42,5 +48,77 @@ public abstract class BaseSimpleNoDirectToDBEditor<POJO> extends BaseSimpleEdito
 		dataContainer.appendNewItem(data);
 		
 	}
+
+	@Override
+	public void deleteData(POJO data) throws Exception {
+		dataContainer.eraseData(data);
+	}
 	
+	@Listen("onClick = #btnSave")
+	public void saveClick(final Event evt) {
+
+		String confirmMsg = (String)getSelf().getAttribute("confirmationMsg");
+		if(confirmMsg != null && confirmMsg.trim().length() > 0) {
+			
+			Messagebox.show(confirmMsg, "Prompt", 
+					Messagebox.YES|Messagebox.NO, 
+					Messagebox.QUESTION, 
+			new EventListener<Event>() {
+				
+				@Override
+				public void onEvent(Event event) throws Exception {
+					switch(((Integer)event.getData()).intValue()) {
+					case Messagebox.YES:
+						saveData(evt);
+						break;
+					case Messagebox.NO:
+						break;
+					}
+				}
+			});				
+		} else saveData(evt); 
+	}
+	
+	@Listen("onClick = #btnCancel")
+	public void cancelClick() {
+		String cancelMsg = (String)getSelf().getAttribute("cancellationMsg");
+		if(cancelMsg != null && cancelMsg.trim().length() > 0) {
+			
+			Messagebox.show(cancelMsg, "Prompt", 
+					Messagebox.YES|Messagebox.NO, 
+					Messagebox.QUESTION, 
+			new EventListener<Event>() {
+				
+				@Override
+				public void onEvent(Event event) throws Exception {
+					switch(((Integer)event.getData()).intValue()) {
+					case Messagebox.YES:
+						EditorManager.getInstance().closeCurrentEditorPanel();
+						break;
+					case Messagebox.NO:
+						break;
+					}
+				}
+			});				
+		} else EditorManager.getInstance().closeCurrentEditorPanel();
+	}
+
+	private final void saveData(final Event event) {
+		parseEditedData(event.getTarget());
+		try {
+			bindValueFromControl(getEditedData());
+		} catch (Exception e) {
+			 Messagebox.show("Gagal Bind data. error : " + e.getMessage(), "Gagal Bind Data", Messagebox.OK, Messagebox.ERROR);
+			return  ; 
+		}
+		
+		try {
+			if ( ZKEditorState.ADD_NEW.equals(getEditorState())) {
+				insertData();
+			} else if(ZKEditorState.EDIT.equals(getEditorState())) {
+				updateData();
+			}
+		} catch (Exception e) {
+		}
+	}	
 }
