c57dc72c4da41e1703f87387e35f1e2616bc91ad
Pala Windu <pala.windu@gmail.com>
2014-10-09 10:26:46 +0800

    update code

--- lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/controller/master/SystemParameterController.java
+++ lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/controller/master/SystemParameterController.java
@@ -1,28 +1,21 @@
 package id.co.sigma.zk.ui.controller.master;
 
+import id.co.sigma.common.data.app.SystemSimpleParameter;
+import id.co.sigma.common.data.query.SimpleQueryFilterOperator;
+import id.co.sigma.common.server.service.IGeneralPurposeService;
+import id.co.sigma.zk.ui.annotations.QueryParameterEntry;
+import id.co.sigma.zk.ui.controller.IReloadablePanel;
+import id.co.sigma.zk.ui.controller.base.BaseSimpleListController;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.Executions;
-import org.zkoss.zk.ui.event.ForwardEvent;
-import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Listitem;
-import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Textbox;
-
-import id.co.sigma.common.data.query.SimpleQueryFilterOperator;
-import id.co.sigma.common.security.domain.PageDefinition;
-import id.co.sigma.common.server.service.IGeneralPurposeService;
-import id.co.sigma.common.data.app.SystemSimpleParameter;
-import id.co.sigma.zk.ui.annotations.QueryParameterEntry;
-import id.co.sigma.zk.ui.controller.EditorManager;
-import id.co.sigma.zk.ui.controller.IReloadablePanel;
-import id.co.sigma.zk.ui.controller.base.BaseSimpleListController;
 
 /**
  * 
@@ -49,14 +42,7 @@ public class SystemParameterController extends BaseSimpleListController<SystemSi
 	    Button searchButton ;
 	    @Wire
 	    Listbox listbox ; 
-	    @Wire Button resetButton ;
-		@Wire Button addButton ; 
-
-		@Wire Button btnEdit ; 
-		@Wire Button btnHapus ;
- 
-
-	
+	    
 	    @Autowired
 		IGeneralPurposeService generalPurposeService;
 	    
@@ -69,67 +55,27 @@ public class SystemParameterController extends BaseSimpleListController<SystemSi
 	        super.doAfterCompose(comp);
 	    }
 
-	    @Listen(value="onClick = #searchButton")
-	    public void click() {
-	        invokeSearch();
-	    };
-
 	    @Override
 	    public Listbox getListbox() {
 	        return listbox;
 	    }
-	    
-	    @Listen(value="onClick = #addButton")
-	    public void clickAdd(){
-	    	SystemSimpleParameter simpleParameter = new SystemSimpleParameter();
-	    	EditorManager.getInstance().addNewData("~./zul/pages/master/SystemParameterFormEditor.zul", simpleParameter, this);
-	    }
-	    
-	    /*@Listen("onEdit=#listbox")
-		public void onEdit(ForwardEvent event){
-			Button btn = (Button)event.getOrigin().getTarget();
-			Listitem item = (Listitem)btn.getParent().getParent();
-			SystemSimpleParameter simpleParameter = (SystemSimpleParameter)item.getValue();
-			Executions.getCurrent().setAttribute("system", simpleParameter);
-			EditorManager.getInstance().editData("~./zul/pages/master/SystemParameterFormEditor.zul", simpleParameter, this);
-		}
-	   
-	    
-	 	@Listen("onDelete=#listbox")
-		public void onDelete(ForwardEvent event) {
-			try {
-				Button btn = (Button)event.getOrigin().getTarget();
-				Listitem item = (Listitem)btn.getParent().getParent();
-				SystemSimpleParameter system = (SystemSimpleParameter)item.getValue();
-				
-				SystemSimpleParameter deleteParameter =  generalPurposeDao.get(SystemSimpleParameter.class, system.getId()); 
-				generalPurposeService.delete( deleteParameter);
-				
-				invokeSearch();
-				Messagebox.show("Data berhasil dihapus");
-			} catch (Exception e) {
-				e.printStackTrace();
-				Messagebox.show("Gagal menghapus data!");
-			}
-		}*/
-	    
 	    
 		@Override
 		public void reload() {
 			invokeSearch();
 		}
 		
-		@Listen(value="onClick = #resetButton")
-		public void resetClick() {
-			keySearch.setValue("");
-			remarkSearch.setValue("");
-			invokeSearch();
-		};
-		
 		@Override
 		public void deleteData(SystemSimpleParameter data) {
 			deleteData(data, data.getId(), "id");
 			reload();
+		}
+		/* (non-Javadoc)
+		 * @see id.co.sigma.zk.ui.controller.base.BaseSimpleListController#addNewData()
+		 */
+		@Override
+		public SystemSimpleParameter addNewData() {
+			return new SystemSimpleParameter();
 		}
 
 	    
