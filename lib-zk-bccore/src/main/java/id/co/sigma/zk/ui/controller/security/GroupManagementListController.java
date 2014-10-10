00bf3930d8d468f14e92fc17e9165d376924ba78
goesderai <goesderai@gmail.com>
2014-10-10 14:51:26 +0800

    Bug fixing

--- lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/controller/security/GroupManagementListController.java
+++ lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/controller/security/GroupManagementListController.java
@@ -1,14 +1,18 @@
 package id.co.sigma.zk.ui.controller.security;
 
+import id.co.sigma.common.data.query.SimpleQueryFilter;
 import id.co.sigma.common.data.query.SimpleQueryFilterOperator;
 import id.co.sigma.common.security.domain.UserGroup;
+import id.co.sigma.common.security.domain.UserGroupAssignment;
 import id.co.sigma.zk.ui.annotations.QueryParameterEntry;
 import id.co.sigma.zk.ui.controller.IReloadablePanel;
 import id.co.sigma.zk.ui.controller.base.BaseSimpleListController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.zkoss.util.resource.Labels;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Textbox;
 
 /**
@@ -23,6 +27,8 @@ public class GroupManagementListController extends BaseSimpleListController<User
 	@Qualifier(value="securityApplicationId")
 	@Autowired
 	private String applicationId;
+	
+	private final String moduleTitle = "Daftar Group Menu";
 	
 	@Wire private Listbox pageListBox;
 	
@@ -50,6 +56,24 @@ public class GroupManagementListController extends BaseSimpleListController<User
 	@Override
 	public UserGroup addNewData() {
 		return new UserGroup();
+	}
+	
+	private boolean groupIsInUse(Long groupId){
+		SimpleQueryFilter[] filters = {
+			new SimpleQueryFilter("groupId", SimpleQueryFilterOperator.equal, groupId)
+		};
+		Long nGroupAssigns = generalPurposeDao.count(UserGroupAssignment.class, filters);
+		return (nGroupAssigns!=null && nGroupAssigns > 0);
+	}
+
+	@Override
+	public void deleteData(UserGroup data) {
+		if(groupIsInUse(data.getId())){
+			String msg = Labels.getLabel("");
+			Messagebox.show("Data group menu tidak bisa dihapus karena masih digunakan oleh user", moduleTitle, Messagebox.OK, Messagebox.EXCLAMATION);
+		}else{
+			super.deleteData(data, data.getId(), "id");
+		}
 	}
 	
 }
