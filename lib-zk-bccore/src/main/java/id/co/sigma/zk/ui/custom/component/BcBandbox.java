a0ed4fbb6f85395426004edf34291359eddc8f33
goesderai <goesderai@gmail.com>
2014-10-20 09:20:23 +0800

    New custom component (bandbox)
    komponen bandbox dgn fungsi autocomplete

---
+++ lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/custom/component/BcBandbox.java
@@ -0,0 +1,21 @@
+package id.co.sigma.zk.ui.custom.component;
+
+import org.zkoss.zk.ui.IdSpace;
+import org.zkoss.zk.ui.ext.AfterCompose;
+import org.zkoss.zul.Bandbox;
+
+/**
+ * Tulis keterangan class di sini
+ * @author <a href="mailto:ida.suartama@sigma.co.id">Goesde Rai</a>
+ */
+public class BcBandbox extends Bandbox implements IdSpace, AfterCompose{
+	
+	private static final long serialVersionUID = -3806592381198871322L;
+	
+	static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(BcBandbox.class.getName());
+
+	@Override
+	public void afterCompose() {
+		// TODO Auto-generated method stub
+	}
+}
