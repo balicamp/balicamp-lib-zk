93b8d1b0d77736542379efa5701faa61762d72a7
palawindu <pala.windu@gmail.com>
2014-10-30 16:50:51 +0800

    update code

--- lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/custom/component/ListOfValueComboitemRenderer.java
+++ lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/custom/component/ListOfValueComboitemRenderer.java
@@ -1,15 +1,28 @@
 package id.co.sigma.zk.ui.custom.component;
 
+import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Comboitem;
 import org.zkoss.zul.ComboitemRenderer;
 
 public class ListOfValueComboitemRenderer implements ComboitemRenderer<ListOfValueItem>{
+
+	private String defaultValue;
+	
+	public ListOfValueComboitemRenderer(String defaultValue) {
+		super();
+		this.defaultValue = defaultValue;
+	}
 
 	@Override
 	public void render(Comboitem item, ListOfValueItem data, int index)
 			throws Exception {
 		item.setLabel(data.toString());
 		item.setValue(data.getValue());
+		if(data.getValue() != null) {
+			if((item.getParent() instanceof Combobox) && (data.getValue().toString().equals(defaultValue))) {
+				((Combobox)item.getParent()).setValue(item.getLabel());		
+			}
+		}
 	}
 
 }
