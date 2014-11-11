01727a5ad72956afc4ca2c4e39aa9377036cba8b
palawindu <pala.windu@gmail.com>
2014-11-11 15:25:20 +0800

    update renderer

--- lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/custom/component/ListOfValueComboitemRenderer.java
+++ lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/custom/component/ListOfValueComboitemRenderer.java
@@ -7,10 +7,12 @@ import org.zkoss.zul.ComboitemRenderer;
 public class ListOfValueComboitemRenderer implements ComboitemRenderer<ListOfValueItem>{
 
 	private String defaultValue;
+	private boolean found = false;
 	
 	public ListOfValueComboitemRenderer(String defaultValue) {
 		super();
 		this.defaultValue = defaultValue;
+		this.found = false;
 	}
 
 	@Override
@@ -20,9 +22,10 @@ public class ListOfValueComboitemRenderer implements ComboitemRenderer<ListOfVal
 		item.setValue(data.getValue());
 		if(data.getValue() != null) {
 			if((item.getParent() instanceof Combobox) && (data.getValue().toString().equals(defaultValue))) {
-				((Combobox)item.getParent()).setValue(item.getLabel());		
+				((Combobox)item.getParent()).setValue(item.getLabel());
+				this.found = true;
 			} else if(item.getParent() instanceof Combobox) {
-				((Combobox)item.getParent()).setValue("");
+				if(!this.found) ((Combobox)item.getParent()).setValue("");
 			}
 		}
 	}
