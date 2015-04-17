5a48859503df5cd2f6e6bdfff017314a65619307
goesderai <goesderai@gmail.com>
2015-04-17 16:32:06 +0800

    Register new component (COA suggestion box)

---
+++ lib-zk-bccore/src/main/java/id/co/sigma/zk/ui/component/CoaSuggestionBox.java
@@ -0,0 +1,19 @@
+package id.co.sigma.zk.ui.component;
+
+import org.zkoss.zk.ui.ext.AfterCompose;
+import org.zkoss.zul.Bandbox;
+
+/**
+ * COA Suggestion box
+ * @author <a href="mailto:ida.suartama@sigma.co.id">Goesde Rai</a>
+ */
+public class CoaSuggestionBox extends Bandbox implements AfterCompose{
+	private static final long serialVersionUID = -5852112816418552173L;
+	
+	static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CoaSuggestionBox.class.getName());
+
+	@Override
+	public void afterCompose() {
+		// TODO Auto-generated method stub
+	}
+}
